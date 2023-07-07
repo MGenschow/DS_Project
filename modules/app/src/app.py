dcfbf83c22ddba9ea705577413346be362fae369
Stefan <stefan.grochowski@gmx.de>
2023-07-07 16:04:28 +0200

    Changed file names

--- modules/app/src/app.py
+++ modules/app/src/app.py
@@ -3,98 +3,88 @@ from dash import html, dcc, callback, Input, Output, State
 import dash_bootstrap_components as dbc
 import os
 
-from footer import footer
+from footer_2 import footer
 from navbar import navbar
 
-mode = "fre"
 
-# Build the app
 app = dash.Dash(
     __name__,
     use_pages=True,
-    external_stylesheets=[dbc.themes.SOLAR],
+    external_stylesheets=[dbc.themes.LUX, dbc.icons.FONT_AWESOME],
     meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
 )
-server = app.server
 
-# --------------------
-# SIDEBAR STYLE
-# --------------------
+# Assuming you have defined the `dash` and `page_registry` variables
 
-# Define the style for the sidebar and the content
-SIDEBAR_STYLE = {
-    "position": "fixed",
-    "top": 55,
-    "left": 0,
-    "bottom": 0,
-    "width": "16rem",
-    "padding": "2rem 1rem",
-    "background-color": "#456789",
-    "zIndex": 0,  # Set the sidebar above the content
-}
-
-# Padding for the page content
-CONTENT_STYLE = {
-    "margin-left": "18rem",
-    "margin-right": "2rem",
-    "padding": "2rem 1rem",
-    "zIndex": 0,  # Set the content below the sidebar
-}
-
-sidebar = sidebar = html.Div([
-        html.H2("Sidebar", className="display-4"),
-        html.Hr(),
-        html.P("Menu", className="lead"),
-        dbc.Nav(
-            [
-                dbc.NavLink(
+sidebar = html.Div(
+    [
+        html.Div(
+            [   
+                html.Br(),
+                html.Hr(),
+                html.P("Menu", className="lead"),
+                dbc.Nav(
                     [
-                        html.Div(page["name"], className="ms-2"),
+                        dbc.NavLink(
+                            [
+                                html.I(className=page.get("icon")),
+                                html.Span(page["name"], className="ms-3"),
+                            ],
+                            href=page["path"],
+                            active="exact",
+                            className="custom-navlink",
+                        )
+                        for page in dash.page_registry.values()
                     ],
-                    href=page["path"],
-                    active="exact",
+                    vertical=True,
+                    pills=True,
+                    #className="nav-list-items",
                 )
-                for page in dash.page_registry.values()
             ],
-            vertical=True,
-            pills=True,
-            className="bg-light",
-            )], 
-            style=SIDEBAR_STYLE,
+            className="sidebar",
+        )
+    ]
 )
 
-app.layout = dbc.Container([
-    dbc.Row(navbar), 
-    dbc.Row([
-        dbc.Col(html.Div("HeatMapper - Unveiling Munich's Hidden Heat Islands",
-                         style={'fontSize':50, 'textAlign':'center'}))
-    ]),
 
-    html.Hr(),
 
-    dbc.Row(
-        [
-            dbc.Col(
-                [
-                    sidebar
-                ], xs=4, sm=4, md=2, lg=2, xl=2, xxl=2, className="sticky-top"),
-
-            dbc.Col(
-                [
-                    dash.page_container
-                ], xs=8, sm=8, md=10, lg=10, xl=10, xxl=10)
-        ]
-    ), 
-    dbc.Row(footer)
-    ], 
-    fluid=True
+app.layout = html.Div(
+    style={"overflow": "auto"},
+    children=[
+        navbar,
+        dbc.Container(
+            [   
+                #dbc.Row(navbar),
+                html.Br(),
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            [sidebar],
+                            xs=3,
+                            sm=3,
+                            md=1,
+                            lg=1,
+                            xl=1,
+                            xxl=1,
+                            className="sticky-top",
+                        ),
+                        dbc.Col(
+                            [dash.page_container],
+                            xs=9,
+                            sm=9,
+                            md=11,
+                            lg=11,
+                            xl=11,
+                            xxl=11,
+                        ),
+                    ]
+                ),
+            ],
+            fluid=True,
+        ),
+        footer
+    ],
 )
-
 
 if __name__ == "__main__":
-    if mode == "dev":
-        app.run_server(debug=True, port=8050)
-    else:
-        app.run_server(host='0.0.0.0')
-
-
+    app.run_server(debug=True)
